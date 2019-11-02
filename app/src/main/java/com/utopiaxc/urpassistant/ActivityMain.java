1b41fbe2767b5a4262f175a12c346e5e43e75c8c
杜宇森 <dys1025@sina.com>
2019-11-03 00:12:41 +0800

    函数结构优化

--- app/src/main/java/com/utopiaxc/urpassistant/ActivityMain.java
+++ app/src/main/java/com/utopiaxc/urpassistant/ActivityMain.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.utopiaxc.urpassistant.fragments.FragmentHome;
+import com.utopiaxc.urpassistant.fuctions.FuctionsPublicBasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
@@ -29,6 +30,7 @@ public class ActivityMain extends AppCompatActivity {
 
     private FragmentHome fragmentHome;
     private String updateCheak="";
+    private FuctionsPublicBasic basicFuctions = new FuctionsPublicBasic();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -61,35 +63,15 @@ public class ActivityMain extends AppCompatActivity {
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .add(R.id.frameLayout, fragmentHome)
                 .commitAllowingStateLoss();
+
+        //开启更新检查线程
         new Thread(new checkupdateRunnable()).start();
+
     }
-    //检查更新
-    class checkupdateRunnable implements Runnable {
-        @Override
-        public void run() {
-            try {
-                PackageManager packageManager = getPackageManager();
-                PackageInfo packInfo = packageManager.getPackageInfo(getPackageName(), 0);
-                String version = packInfo.versionName;
-
-                String latest_version = getHTML("https://raw.githubusercontent.com/UtopiaXC/URPAssistant/master/version_control_debug");
 
 
-                if (latest_version.equals("error")) {
-                    updateCheak = "";
-                } else if (latest_version.equals(version)) {
-                    updateCheak = "";
-                } else {
-                    updateCheak = (String) getText(R.string.has_update);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            messageHandler.sendMessage(messageHandler.obtainMessage());
-        }
-    };
 
-
+    //异步消息同步
     @SuppressLint("HandlerLeak")
     private Handler messageHandler = new Handler() {
         @Override
@@ -121,34 +103,32 @@ public class ActivityMain extends AppCompatActivity {
         }
     };
 
-    //爬取网页源码方法
-    public String getHTML(String version_address) {
-        System.out.println(version_address);
-        URL url;
-        int responsecode;
-        HttpURLConnection urlConnection;
-        BufferedReader reader;
-        String line;
-        try {
-            url = new URL(version_address);
-            //打开URL
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestProperty("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
-            //获取服务器响应代码
-            responsecode = urlConnection.getResponseCode();
-            if (responsecode == 200) {
-                //得到输入流，即获得了网页的内容
-                reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-                String result = "";
-                while ((line = reader.readLine()) != null) {
-                    result += line;
+    //启动时检查更新
+    class checkupdateRunnable implements Runnable {
+        @Override
+        public void run() {
+            try {
+                PackageManager packageManager = getPackageManager();
+                PackageInfo packInfo = packageManager.getPackageInfo(getPackageName(), 0);
+                String version = packInfo.versionName;
+
+
+                String latest_version = basicFuctions.getHTML("https://raw.githubusercontent.com/UtopiaXC/URPAssistant/master/version_control_debug");
+
+
+                if (latest_version.equals("error")) {
+                    updateCheak = "";
+                } else if (latest_version.equals(version)) {
+                    updateCheak = "";
+                } else {
+                    updateCheak = (String) getText(R.string.has_update);
                 }
-                return result;
-            } else {
-                return "error";
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            return "error";
+            messageHandler.sendMessage(messageHandler.obtainMessage());
         }
-    }
+    };
+
+
 }
---
+++ app/src/main/java/com/utopiaxc/urpassistant/fuctions/FuctionsPublicBasic.java
@@ -0,0 +1,40 @@
+package com.utopiaxc.urpassistant.fuctions;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FuctionsPublicBasic {
+    //爬取网页源码方法
+    public String getHTML(String address) {
+        System.out.println(address);
+        URL url;
+        int responsecode;
+        HttpURLConnection urlConnection;
+        BufferedReader reader;
+        String line;
+        try {
+            url = new URL(address);
+            //打开URL
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+            //获取服务器响应代码
+            responsecode = urlConnection.getResponseCode();
+            if (responsecode == 200) {
+                //得到输入流，即获得了网页的内容
+                reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+                String result = "";
+                while ((line = reader.readLine()) != null) {
+                    result += line;
+                }
+                return result;
+            } else {
+                return "error";
+            }
+        } catch (Exception e) {
+            return "error";
+        }
+    }
+
+}
